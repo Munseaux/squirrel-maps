06767f600f30e9ed66b87e2ec1d3329698cc1f2d
Lucas Munson <lucasbmunson@gmail.com>
2021-07-26 21:10:33 -0400

    some new ideas

--- static/js/elections.js
+++ static/js/elections.js
@@ -1,1 +1,5 @@
 console.log("poop");
+
+
+//squirrel sightings in november vs baseline. 
+//squirrel location in november vs baseline
--- static/js/expansion.js
+++ static/js/expansion.js
@@ -1,1 +1,8 @@
 console.log("poop");
+
+// % of squirrels in the various districts. 
+//% of squirrels shopping (in a store)
+// % of squirrels out to dinner (in a resturant)
+// % of squirrels visitng a museum
+//look at categories of places on google and determine what good categories would be. 
+
