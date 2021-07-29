d2d6348b186aad8b6b841ba0a68ce8f72fbd33eb
Lucas Munson <lucasbmunson@gmail.com>
2021-07-28 20:10:41 -0400

    progress on squirrel extrapolation

--- static/js/expansion.js
+++ static/js/expansion.js
@@ -50,29 +50,55 @@ d3.json(url).then(data => {
         console.log(lon);
         console.log(lat);
         
+        let latMeters = 0;
+        let lonMeters = 0; //number of meters to move the point for both lat and lon. 
+        let r_earth = 6378.137;
+        let pi = Math.PI;
+        let m = (1 / ((2 * pi / 360) * r_earth)) / 1000; //1 meter in degrees.
+
+        //update meters depending on how close the point is to the center of central park 
+
+        if (lat > centerLat){
+            latMeters = 500;
+        } else {
+            latMeters = -500;
+        }
+        if (lon > centerLon){
+            lonMeters = 500;
+        } else {
+            lonMeters = -500;
+        }
+
+        let newLat  = lat + (latMeters * m);
+        let newLon = lon + (lonMeters * m) / Math.cos(lat * (pi / 180));
+
+
         //desperate attempts to make the lon and lat scale properly
         // lon = String(lon).slice(0,4) + Math.round((String(lon).slice(5,16) * .5))
         // lat = String(lat).slice(0,4) + Math.round((String(lat).slice(5,16) * .5))
-        if (lat < centerLat) {
-            lat = lat + .01111111111;
-        } else if (lat > centerLat) {
-            lat = lat - .01111111111;
-        } else {
-            lat = lat;
-        }
 
-        if (lon < centerLon){
-            lon = lon - .01111111111;
-        } else if(lon > centerLon){
-            lon = lon + .01111111111;
-        } else {
-            lon = lon;
-        }
+        //number of km per degree of longitude: (pi/180) * r_earth * cos(theta*pi/180) where theta is latitude in degress and r_earth = 6378km
+
+        //number of km per degree of latitude: (pi/180) * r_earth = 111 km / degree AKA roughly the same at all locations. 
+
+        //therefore: 
+
+        // new_latitude  = latitude  + (dy / r_earth) * (180 / pi);
+        // new_longitude = longitude + (dx / r_earth) * (180 / pi) / cos(latitude * pi/180);
+
+        //so therefore: 
+
+        // number of km per degree = ~111km (111.32 in google maps, but range varies
+        //between 110.567km at the equator and 111.699km at the poles)
+        // 1km in degree = 1 / 111.32km = 0.0089
+        // 1m in degree = 0.0089 / 1000 = 0.0000089
+
+        
 
         console.log(lon);
         console.log(lat);
 
-        var marker = L.marker([lat,lon]).bindPopup(date);
+        var marker = L.marker([newLat,newLon]).bindPopup(`<h3>${date}</h3>`);
         expandArr.push(marker);
     });
 
