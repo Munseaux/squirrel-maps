1ea5c48fb8f33a5741453fe40b70893d6c0a3e87
Lucas Munson <lucasbmunson@gmail.com>
2021-07-30 21:08:08 -0400

    flask app

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, redirect
+from bs4 import BeautifulSoup
+import requests
+
+app = Flask(__name__)
+
+#primary routes
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/elections")
+def elections():
+    return render_template("elections.html")
+
+@app.route("/expansion")
+def expansion():
+    return render_template("expansion.html")
+
+
+#redirects and workarounds
+@app.route("/static/templates/index.html")
+def index_redirect():
+    return redirect("/", code=302)
+@app.route("/index.html")
+def index_redirect_2():
+    return redirect("/", code=302)
+
+@app.route("/static/templates/elections.html")
+def elections_redirect():
+    return redirect("/elections", code=302)
+@app.route("/elections.html")
+def elections_redirect_2():
+    return redirect("/elections", code=302)
+
+@app.route("/static/templates/expansion.html")
+def expansion_redirect():
+    return redirect("/expansion", code=302)
+@app.route("/expansion.html")
+def expansion_redirect_2():
+    return redirect("/expansion", code=302)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
